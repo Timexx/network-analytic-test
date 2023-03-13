1228e37a08678f2a9f2e16379885f3bb22bd309d
Tim <tim.pollmer@gmail.com>
2023-03-13 14:36:37 +0100

    Create test-downloads.sh

---
+++ test-downloads.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# Pfad zum Ergebnis-Datei
+RESULTS_FILE="/var/www/html/mychart/results.csv"
+
+# Prüfen, ob die Datei vorhanden ist. Falls nicht, erstellen.
+if [ ! -f "$RESULTS_FILE" ]; then
+    echo "Erstelle Datei $RESULTS_FILE"
+    echo "Zeitstempel,Erfolgreich" > "$RESULTS_FILE"
+fi
+
+# Herunterladen der Datei und Verarbeitung des Ergebnisses
+echo "Lade Datei herunter..."
+if wget http://ipv4.download.thinkbroadband.com/20MB.zip; then
+    echo "Download erfolgreich."
+    # Erfolgreicher Download, Zeitstempel und Wert 1 in CSV-Datei schreiben
+    current_time=$(date "+%Y-%m-%d %H:%M:%S")
+    echo "$current_time;1" >> "$RESULTS_FILE"
+else
+    echo "Download fehlgeschlagen."
+    # Fehlgeschlagener Download, Zeitstempel und Wert 0 in CSV-Datei schreiben
+    current_time=$(date "+%Y-%m-%d %H:%M:%S")
+    echo "$current_time;0" >> "$RESULTS_FILE"
+fi
+
+# Löschen der heruntergeladenen Datei
+rm 20MB.zip
+
+# Pfad zum Ziel-Verzeichnis
+TARGET_DIR="/var/www/html/mychart"
+
+# Ergebnis-Datei in das Ziel-Verzeichnis kopieren
+cp "$RESULTS_FILE" "$TARGET_DIR"
+
+# Gnuplot-Skript erstellen
+#gnuplot_script="set terminal svg enhanced background rgb 'white';
+#                set output '$TARGET_DIR/results.svg';
+#                set boxwidth 1;
+#                set style fill solid;
+#                set yrange [-0.1:1.1];
+#                set xlabel 'Zeitstempel';
+#                set ylabel 'Download-Status';
+#                set xdata time;
+#                set timefmt '%Y-%m-%d %H:%M:%S';
+#                set format x '%d-%m %H:%M';
+#                plot '$RESULTS_FILE' using 1:2:(0.4) with boxes lc rgb 'blue' notitle"
+
+# Gnuplot aufrufen und Diagramm erstellen
+#gnuplot -e "$gnuplot_script"
+
+#echo "Diagramm wurde erstellt und unter $TARGET_DIR/results.svg gespeichert."
+echo "Daten generiert und transferiert"
